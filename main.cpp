0d4c707526bb3848b4cb9fc463e7143930da18a9
Luis <luis.avila@sjsu.edu>
2019-05-01 11:28:34 -0700

    Parsing done.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <fstream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ifstream file ("sample-jobs.dat");
+
+    if(file.is_open()){
+        cout << "File opened correctly." << endl;
+    }
+    else{
+        cout << "Failed" << endl;
+    }
+
+    int ID[100];
+    char Action[100];
+    char Page[100];
+
+    int i = 0;
+    cout << "PID" << "\t" << "Act" << "\t" << "Page" << endl;
+
+    while(!file.eof()){
+        file >> ID[i] >> ws >> Action[i] >> ws;
+
+        if(Action[i] == 'C' || Action[i] == 'T'){
+            Page[i] = NULL;
+        }
+        else{
+            file >> Page[i];
+        }
+
+        cout << ID[i] << "\t" << Action[i] << "\t" << Page[i] << endl;
+
+        if(i == 99){
+            break;
+        }
+        i++;
+    }
+
     return 0;
 }
